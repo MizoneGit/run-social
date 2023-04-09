5b600f904b45469f4c06618e92b139470db529e6
Valery Shcherbinin <fundroin@mail.ru>
2023-04-09 18:04:46 +0500

    Импортирует store и vueTippy в main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import VueTippy from 'vue-tippy'
 
 import './assets/main.css'
+import 'tippy.js/dist/tippy.css'
 
 const app = createApp(App)
 
-app.use(router)
+app
+    .use(store)
+    .use(router)
+    .use(VueTippy, {
+        position: 'bottom-center',
+        timeout: 3000,
+        draggable: true,
+        draggablePercent: 0.7,
+        pauseOnHover: true,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: false,
+        icon: true,
+        transition: 'Vue-Toastification__bounce',
+        maxToasts: 5,
+        newestOnTop: true,
+    });
 
 app.mount('#app')
