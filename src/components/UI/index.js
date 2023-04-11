0dc144cb335da51025f87106870917cf0f93765b
Valery Shcherbinin <fundroin@mail.ru>
2023-04-11 12:54:49 +0500

    Регистрирует UI компоненты глобально

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,3 +1,9 @@
-export default [
+import VButton from "@/components/UI/VButton.vue";
+import VInput from "@/components/UI/VInput.vue";
+import VSelect from "@/components/UI/VSelect.vue";
 
+export default [
+    VButton,
+    VInput,
+    VSelect
 ]
