45748c91e74bfb7fbf19226caf015e45232b2433
Valery Shcherbinin <fundroin@mail.ru>
2023-04-11 10:35:41 +0500

    Регистрирует библиотеки и переподключает стили

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import router from './router'
 import store from './store'
 import VueTippy from 'vue-tippy'
 import uiComponents from '@/components/UI/index'
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
-import './assets/main.css'
+import './assets/main.scss'
 import 'tippy.js/dist/tippy.css'
 
 const app = createApp(App)
-
+app.component('EasyDataTable', Vue3EasyDataTable);
 uiComponents.forEach(component => {
     app.component(component.name, component);
 });
