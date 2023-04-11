ebca826d0f748059a86d2afc778520102fc25473
Valery Shcherbinin <fundroin@mail.ru>
2023-04-11 12:53:29 +0500

    Добавляет UI компоненты

---
+++ src/components/UI/VButton.vue
@@ -0,0 +1,64 @@
+<template>
+  <button :class="['v-button', `v-button--${color}`]">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "VButton",
+  props: {
+    text: {
+      type: String,
+      default: 'Button'
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .v-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 20px;
+    gap: 8px;
+    border: 2px solid transparent;
+    border-radius: 12px;
+    cursor: pointer;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 24px;
+    letter-spacing: -0.01em;
+    transition: border 0.3s, color 0.3s, background-color 0.3s;
+
+    &:disabled {
+      pointer-events: none;
+      opacity: 0.5;
+    }
+
+    &--primary {
+      background-color: var(--primary-blue-color);
+      color: var(--primary-champagne-color);
+
+      &:hover {
+        background-color: var(--primary-dark-blue-color);
+      }
+    }
+
+    &--secondary {
+      background-color: var(--neutral-light-black-background-color);
+      border-color: var(--neutral-dark-gray-background-color);
+      color: var(--neutral-champagne-color);
+
+      &:hover {
+        background-color: transparent;
+        border-color: var(--neutral-champagne-background-color);
+      }
+    }
+  }
+</style>
